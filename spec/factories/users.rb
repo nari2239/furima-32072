b6e4f2f6d8f0b327107c7acc5f62932900c703bb
nari2239 <oryolife@gmail.com>
2020-11-19 22:28:08 +0900

    FactoryBotの定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {Faker::Name.initials(number: 2)}
+    email                  {Faker::Internet.free_email}
+    password               {Faker::Internet.password(min_length: 6)}
+    password_confirmation  {password}
+    last_name              {Gimei.last.kanji}
+    first_name             {Gimei.first.kanji}
+    last_name_reading      {Gimei.last.katakana}
+    first_name_reading     {Gimei.first.katakana}
+    birth_date             {"2010-01-01"}
+  end
+end
