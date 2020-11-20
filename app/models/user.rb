6a71d7e8347a305f83b588b955936e13f5053f4a
nari2239 <oryolife@gmail.com>
2020-11-20 12:30:29 +0900

    正規表現の変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,12 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    japanese_characters = /\A[ぁ-んァ-ン一-龥]/
+    validates :last_name, format: { with: japanese_characters, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: japanese_characters, message: 'is invalid. Input full-width characters.' }
+    japanese_katakana = /\A[ァ-ヶー－]+\z/
+    validates :last_name_reading, format: { with: japanese_katakana, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_reading, format: { with: japanese_katakana, message: 'is invalid. Input full-width katakana characters.' }
     validates :birth_date
   end
 
