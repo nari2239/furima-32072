81745c74cf3e836c72953a27bd2a9363455c51d8
nari2239 <oryolife@gmail.com>
2020-11-21 03:16:12 +0900

    priceカラムのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,9 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :content
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }
-    validates :price, format: { with: greater_than_or_equal_to: 300, message: 'Out of setting range ' }
-    validates :price, format: { with: less_than_or_equal_to: 9999999, message: 'Out of setting range ' }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters.' }, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range'}
 
-    with_options numericality: { other_than: 1 } do
+    with_options numericality: { other_than: 1, message: 'Select' } do
       validates :category_id
       validates :condition_id
       validates :delivery_fee_id
