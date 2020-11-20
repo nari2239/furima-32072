313d037f45c677b6886f270ce14afead5ea55a96
nari2239 <oryolife@gmail.com>
2020-11-20 19:02:21 +0900

    バリデーションの設定とhas_one_attchedメソッドの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :content
+    validates :price
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :delivery_fee_id
+      validates :prefecture_id
+      validates :period_id
+    end
+  end
+
   belongs_to :user
+  has_one_attached :image
 end
