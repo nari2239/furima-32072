b1ce90daeb270fca8a59ddf41744a00fa5ffed15
nari2239 <oryolife@gmail.com>
2020-11-21 14:08:09 +0900

    テストコードの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    name            {Faker::Commerce.product_name}
+    content         {Faker::Lorem.sentence}
+    category_id     {2}
+    condition_id    {2}
+    delivery_fee_id {2}
+    prefecture_id   {2}
+    period_id       {2}
+    price           {Faker::Commerce.price(range: 300..10000).to_i}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
