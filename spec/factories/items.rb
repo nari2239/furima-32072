749bed663806af197eebae137257f11bef11fb69
nari2239 <oryolife@gmail.com>
2020-11-21 14:35:27 +0900

    FactoryBotのアソシエーションの定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     prefecture_id   {2}
     period_id       {2}
     price           {Faker::Commerce.price(range: 300..10000).to_i}
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
