95f3ff7b6ceb718688d1c91dd1b50e1fe8156b7f
nari2239 <oryolife@gmail.com>
2020-11-22 14:14:46 +0900

    不要な記述の削除とbefore_action呼び出しの順番変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, only: [:edit, :update]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -45,6 +45,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if user_signed_in? && current_user.id != @item.user.id
+    redirect_to root_path if current_user.id != @item.user.id
   end
 end
