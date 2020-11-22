d8b6984a695c9f56cc81cf866cc8028af869ac1e
nari2239 <oryolife@gmail.com>
2020-11-22 21:49:05 +0900

    addressモデルのバリデーションを設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,12 @@
 class Address < ApplicationRecord
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
+    validates :city
+    validates :house_number
+    validates :phone_number, format: { with: /\A\d{1,11}\z/, message: 'Input only number' }
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :order
