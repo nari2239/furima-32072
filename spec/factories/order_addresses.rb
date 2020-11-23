60837140467760266f01322390d19196525bf00c
nari2239 <oryolife@gmail.com>
2020-11-23 21:18:12 +0900

    FactoryBotの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :order_address do
     token         {"tok_abcdefghijk00000000000000000"}
     postal_code   { '123-4567' }
-    prefecture    { 2 }
+    prefecture_id { 2 }
     city          { 'さいたま市' }
     house_number  { '1-1' }
     building_name { 'さいたまハイツ' }
