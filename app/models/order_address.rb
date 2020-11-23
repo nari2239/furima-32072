646fa0e1a257b92bae25a79bece15c9bb85a9471
nari2239 <oryolife@gmail.com>
2020-11-23 16:42:33 +0900

    attr_accessorの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'Input correctly' }
