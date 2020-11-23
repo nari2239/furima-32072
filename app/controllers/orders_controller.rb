6e65e1526617a33aa48367e5a84f5c6389050916
nari2239 <oryolife@gmail.com>
2020-11-23 14:47:31 +0900

    createアクションとストロングパラメーターの追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,21 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAdress.new
+  end
+
+  def create
+    @order_address = OrderAdress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number)
   end
 end
