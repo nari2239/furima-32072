0072f30f5248dc924e770d289b672b8bfc39eaa8
nari2239 <oryolife@gmail.com>
2020-11-23 14:36:53 +0900

    order_addressオブジェクトの編集

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,4 +9,10 @@ class OrderAddress
     validates :house_number
     validates :phone_number, format: { with: /\A\d{1,11}\z/, message: 'Input only number' }
   end
+
+  def save
+    item = Item.find(params[:item_id])
+    order = Order.create(user_id: current_user.id, item_id: item.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
 end
