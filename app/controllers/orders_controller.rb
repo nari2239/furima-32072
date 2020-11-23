3c795e7a129a5f2f15f8afab607f2b69b6121518
nari2239 <oryolife@gmail.com>
2020-11-23 19:32:22 +0900

    ordersコントローラーのリファクタリング

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+ 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = "テスト秘密鍵をここに入れる"
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -22,8 +17,20 @@ class OrdersController < ApplicationController
   end
 
   private
+def set_item
+  @item = Item.find(params[:item_id])
+end
+
   def order_params
-    @item = Item.find(params[:item_id])
     params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "テスト秘密鍵をここに入れる"
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
