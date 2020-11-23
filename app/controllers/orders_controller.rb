ce6b822fdba7c1931b83895bc21f4b7e628a1e5e
nari2239 <oryolife@gmail.com>
2020-11-23 20:32:56 +0900

    自分の出品の購入ページには飛べない様にmove_to_indexを設置

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
  
   def index
     @order_address = OrderAddress.new
@@ -33,5 +34,9 @@ end
       card: order_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user.id
   end
 end
