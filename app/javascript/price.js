c12e905435b1d7a57381c8aa161d6f47c749d2ec
nari2239 <oryolife@gmail.com>
2020-11-24 17:39:50 +0900

    手数料の小数点が生じる時に切り捨てる様に修正

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -4,14 +4,7 @@ function price () {
     const inputPrice = itemPrice.value;
     const taxPrice = inputPrice * 0.1;
     const addTaxPrice = document.getElementById("add-tax-price");
-
-    // 手数料が1円以上なら表示し、1円未満なら0円と表示する条件式
-    if (taxPrice >= 1) {
-      addTaxPrice.innerHTML = taxPrice;
-    } else {
-      addTaxPrice.innerHTML = 0;
-    };
-
+    addTaxPrice.innerHTML = Math.floor(taxPrice);
     const profitField = document.getElementById("profit");
     const profit = inputPrice - addTaxPrice.innerHTML;
     profitField.innerHTML = profit;
